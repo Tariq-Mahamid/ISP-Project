7dd8734634e0684c5e47fc770914bef7466438e6
Ricky Das <ankit.das@student.allenisd.org>
2021-05-13 17:05:27 -0500

    Fixed Meditation Scene bug, updated timing blocks sprites.

--- MeditationScene/EndscreenMS.swift
+++ MeditationScene/EndscreenMS.swift
@@ -2,7 +2,7 @@ import Igis
 import Scenes
 
 
-class EndScreenMS : RenderableEntity{
+class EndScreenMS : RenderableEntity, KeyDownHandler{
     var endScreen = Rectangle(rect: Rect())
     var endScreenWidth : Int
     var endScreenHeight : Int
@@ -31,10 +31,9 @@ class EndScreenMS : RenderableEntity{
     }
 
     override func setup(canvasSize:Size, canvas: Canvas) {
-        
+        dispatcher.registerKeyDownHandler(handler: self)
         endScreenSize = Size(width:endScreenWidth, height:endScreenHeight)
         endScreen = Rectangle(rect: Rect(topLeft: Point(x: (canvasSize.width/2 - endScreenWidth/2), y: (canvasSize.height/2 - endScreenHeight/2)), size: endScreenSize), fillMode: .fillAndStroke)
-
     }
 
     override func render(canvas:Canvas) {
@@ -61,12 +60,33 @@ class EndScreenMS : RenderableEntity{
             canvas.render(FillStyle(color: Color(.orange)), scoreText, endScreenText)
         }
     }
-
+    
     func getPlayerStats() -> PlayerStats {
         guard let mainDirector = director as? ShellDirector else {
             fatalError("mainDirector of type ShellDirector is required")
         }
         return mainDirector.playerStats
+    }
 
+    func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool) {
+        
+        switch code {
+        case "KeyQ":
+            if gameEnded{
+                director.enqueueScene(scene: MainScene())
+                director.transitionToNextScene()
+            }
+        case "KeyR":
+            if gameEnded{
+                director.enqueueScene(scene: MeditationScene())
+                director.transitionToNextScene()
+            }
+        default :
+            print("Unspecified key has been pressed.")
+        }
+    }
+
+    override func teardown() {
+        dispatcher.unregisterKeyDownHandler(handler: self)
     }
 }                          
